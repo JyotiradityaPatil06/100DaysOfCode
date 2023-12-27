6b6eabd9680a2ed430504b6ec4f2d3b6ce77015a
jyotiraditya06 <adityapatil065@gmail.com>
2023-12-27 21:08:46 +0530

    Delete middle element of a stack (GFG)

---
+++ Dat 22 (P2).java
@@ -0,0 +1,63 @@
+// Delete middle element of a stack (GFG)
+
+//{ Driver Code Starts
+//Initial template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int sizeOfStack = sc.nextInt();
+            Stack<Integer> myStack = new Stack<>();
+
+            // adding elements to the stack
+            for (int i = 0; i < sizeOfStack; i++) {
+                int x = sc.nextInt();
+                myStack.push(x);
+
+            }
+            Solution obj = new Solution();
+            obj.deleteMid(myStack, sizeOfStack);
+
+            while (!myStack.isEmpty()) {
+                System.out.print(myStack.peek() + " ");
+                myStack.pop();
+            }
+            System.out.println();
+        }
+
+    }
+}
+// } Driver Code Ends
+
+// User function Template for Java
+
+class Solution {
+    // Function to delete middle element of a stack.
+    public void deleteMid(Stack<Integer> s, int sizeOfStack) {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        if (sizeOfStack % 2 == 0) {
+            for (int i = 0; i < (sizeOfStack + 1) / 2; i++) {
+                stack.push(s.pop());
+            }
+            s.pop();
+            while (!stack.isEmpty()) {
+                s.push(stack.pop());
+            }
+        } else {
+            for (int i = 0; i < (sizeOfStack + 1) / 2 - 1; i++) {
+                stack.push(s.pop());
+            }
+            s.pop();
+            while (!stack.isEmpty()) {
+                s.push(stack.pop());
+            }
+        }
+    }
+}
