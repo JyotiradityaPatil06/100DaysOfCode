1c62772d5899797fb98797a163432f83a4b31cd6
jyotiraditya06 <adityapatil065@gmail.com>
2024-01-03 22:30:08 +0530

    Leetcode 2125. Number of Laser Beams in a Bank

---
+++ Day 29 (P1).java
@@ -0,0 +1,18 @@
+// Leetcode 2125. Number of Laser Beams in a Bank
+
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int ans = 0, prev = 0, count = 0;
+        for (String s : bank) {
+            count = 0;
+            for (int i = 0; i < s.length(); i++)
+                if (s.charAt(i) == '1')
+                    count++;
+            if (count > 0) {
+                ans += prev * count;
+                prev = count;
+            }
+        }
+        return ans;
+    }
+}
